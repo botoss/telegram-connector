9ad58bca3632f2eeca20831ed6c561275994b11c
Evgeny Veretennikov <evg.veretennikov@gmail.com>
2017-07-28 00:15:50 +0300

    Docker configs rename to *.properties, log config

--- src/main/scala/ru/botoss/telegram/DockerEnvironment.scala
+++ src/main/scala/ru/botoss/telegram/DockerEnvironment.scala
@@ -2,11 +2,14 @@ package ru.botoss.telegram
 import java.io.File
 
 import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.scalalogging.StrictLogging
 
-object DockerEnvironment extends Environment {
+object DockerEnvironment extends Environment with StrictLogging {
   override val config: Config = {
-    val pathnames = Seq("/kafka.cfg", "/telegram.cfg", "/run/secrets/telegram.secrets")
-    pathnames.map(parseFile).fold(ConfigFactory.defaultApplication())(_.withFallback(_))
+    val pathnames = Seq("/kafka.properties", "/telegram.properties", "/run/secrets/telegram.properties")
+    val config = pathnames.map(parseFile).fold(ConfigFactory.defaultApplication())(_.withFallback(_))
+    logger.debug(s"config: $config")
+    config
   }
 
   private def parseFile(pathname: String) = ConfigFactory.parseFile(new File(pathname))
