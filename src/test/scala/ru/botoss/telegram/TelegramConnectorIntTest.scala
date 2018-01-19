a2e0a204f2fa3c8eb057a3c896b08b3c02f02679
Evgeny Veretennikov <evg.veretennikov@gmail.com>
2018-01-19 22:25:45 +0300

    #30 always wait for requestHandler() call in integration test

--- src/test/scala/ru/botoss/telegram/TelegramConnectorIntTest.scala
+++ src/test/scala/ru/botoss/telegram/TelegramConnectorIntTest.scala
@@ -5,21 +5,30 @@ import info.mukel.telegrambot4s.methods.SendMessage
 import info.mukel.telegrambot4s.models.{Chat, ChatId, ChatType, Message}
 import net.manub.embeddedkafka.EmbeddedKafka
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.ScalaFutures
 import ru.botoss.telegram.model.{Key, Request, Response}
 import ru.botoss.telegram.serde._
 
+import scala.concurrent.Promise
 import scala.concurrent.duration._
 import scalaz.Show.showFromToString
 
-class TelegramConnectorIntTest extends UnitSpec with EmbeddedKafka with BeforeAndAfterAll {
+class TelegramConnectorIntTest extends UnitSpec with EmbeddedKafka with BeforeAndAfterAll with ScalaFutures {
   implicit private val env = TestEnvironment
   private val requestHandler = mock[RequestHandler]
+  private val proxyTimeout = 10.seconds
   private val bot = TelegramConnectorFactory(
     new Bot(_) {
       override val client: RequestHandler = requestHandler
     },
-    proxyTimeout = 10.seconds
+    proxyTimeout
   )
+  private val responseMessage = Message(
+    messageId = 1,
+    date = 1,
+    chat = Chat(1, ChatType.Private)
+  )
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(proxyTimeout)
 
   override protected def beforeAll(): Unit = {
     EmbeddedKafka.start()
@@ -33,13 +42,19 @@ class TelegramConnectorIntTest extends UnitSpec with EmbeddedKafka with BeforeAn
       text = Some("/testcmd uppercase it")
     ))
 
+    // needed to await for requestHandler.apply() call
+    val promise = Promise[Message]()
     (requestHandler.apply(_: SendMessage)(_: Manifest[Message])).expects(
       SendMessage(ChatId(1), "UPPERCASE IT"), *
-    )
+    ).onCall { _ =>
+      promise.success(responseMessage)
+      promise.future
+    }
 
     val (key, request) = consumeFirstKeyedMessageFrom[Key, Request]("to-module")
     val response = Response(text = request.command.params.map(_.toUpperCase).mkString(" "))
     publishToKafka("to-connector", key, response)
+    promise.future.futureValue
   }
 
   it should "ignore invalid message and then handle request anyway" in {
@@ -50,15 +65,21 @@ class TelegramConnectorIntTest extends UnitSpec with EmbeddedKafka with BeforeAn
       text = Some("/testcmd uppercase it")
     ))
 
+    // needed to await for requestHandler.apply() call
+    val promise = Promise[Message]()
     (requestHandler.apply(_: SendMessage)(_: Manifest[Message])).expects(
       SendMessage(ChatId(1), "UPPERCASE IT"), *
-    )
+    ).onCall { _ =>
+      promise.success(responseMessage)
+      promise.future
+    }
 
     val (key, request) = consumeFirstKeyedMessageFrom[Key, Request]("to-module")
     val response = Response(text = request.command.params.map(_.toUpperCase).mkString(" "))
     implicit val ss = showFromToString[String]
     publishToKafka("to-connector", "invalid-key", response)
     publishToKafka("to-connector", key, response)
+    promise.future.futureValue
   }
 
   override protected def afterAll(): Unit = {
